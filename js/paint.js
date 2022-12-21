12bf6da996d660c532466a3ac2771b0995bf3851
sasmirno <sasmirno1@gmail.com>
2022-12-21 20:49:55 +0300

    Agh

--- js/paint.js
+++ js/paint.js
@@ -1,19 +1,42 @@
-main_div = document.querySelector(".canvas");
-main_div.onmousedown=function(e) {
-	main_div.onmousemove=function(e) {
+canvas = document.querySelector(".canvas");
+color_btn = document.querySelectorAll(".color");
+size_btn = document.querySelectorAll(".size");
+
+// Выбор цвета
+let color = "black";
+for  (i = 0; i < color_btn.length; i++) {
+	color_btn[i].addEventListener("click", function() {
+		color = this.id;
+	});
+}
+// Выбор размера
+let size = "5px";
+for  (i = 0; i < size_btn.length; i++) {
+	size_btn[i].addEventListener("click", function() {
+		size = this.id;
+	});
+}
+
+canvas.onmousedown=function(e) {
+	canvas.onmousemove=function(e) {
 		point (e)
 	}
 }
-main_div.onmouseup=function(e) {
+
+canvas.onmouseup=function(e) {
 	stop()
 }
+
 function point(e){
 	div=document.createElement("div");
 	div.style.left=e.clientX+"px";
 	div.style.top=e.clientY+"px";
-	div.style.background="red";
-	main_div.append(div);
+	div.style.width=size;
+	div.style.height=size;
+	div.style.background=color;
+	canvas.append(div);
 }
+
 function stop() {
-	main_div.onmousemove=0;
+	canvas.onmousemove=0;
 }
