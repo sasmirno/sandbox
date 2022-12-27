ffda4c5c4e9c256e0e401ac3ff994e1b14b5e10d
sasmirno <sasmirno1@gmail.com>
2022-12-27 20:51:28 +0300

    Agh

--- js/tetris.js
+++ js/tetris.js
@@ -1,9 +1,38 @@
 field = document.querySelector(".tetris_field");
 buttons = document.querySelectorAll(".tetris_btn");
-square=document.createElement("div");
+
 
 let x = 125;
 let y = 0;
+
+function creation() {
+	if (y==0) {}
+	square=document.createElement("div");
+	setInterval(move, 500);
+	//figure();
+}
+
+function move() {
+	if (y<500) {
+		square.style.top = y+"px";
+		y += 25;
+		figure();
+	} else {
+		clearInterval(move);
+		y = 0;
+	}
+}
+
+function figure() {
+	square.style.position = "absolute";
+	square.style.left = x+"px";
+	//square.style.top = y+"px";
+	square.style.width = "25px";
+	square.style.height = "25px";
+	square.style.background = "red";
+	field.append(square);
+}
+
 // Кнопки управления
 for (i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function() {
@@ -19,12 +48,8 @@ for (i = 0; i < buttons.length; i++) {
 		}
 	});
 }
-function figure() {
-	square.style.position = "absolute";
-	square.style.left = x+"px";
-	//square.style.top = y+"px";
-	square.style.width = "25px";
-	square.style.height = "25px";
-	square.style.background = "red";
-	field.append(square);
-}
+
+//figure();
+creation();
+
+//setInterval(move, 1000);
