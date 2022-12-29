ff94dad681071aef15ec39a8bad53eb4c0ddf9d8
sasmirno <sasmirno1@gmail.com>
2022-12-29 21:00:39 +0300

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -5,24 +5,27 @@ buttons = document.querySelectorAll(".tetris_btn");
 let x = 125;
 let y = 0;
 let column = 5;
+let row = 0;
 let сolumns = [500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500];
 
 /*function start() {
-	setInterval(move, 500);
+	setInterval(move, 50);
 }*/
 
 function move() {
+	console.log(row);
 	if (y<сolumns[column]) {
 		square.style.top = y+"px";
 		y += 25;
+		row += 1;
 	} else if (сolumns[column] == 0) {
 		console.log('stop');
-		clearInterval(move);
+		clearInterval(timerId);
 	} else {
-		clearInterval(move);
 		сolumns[column] += -25
 		x = 125;
 		y = 0;
+		row = 0;
 		column = 5;
 		squareCreation();
 		//console.log(сolumns);
@@ -44,19 +47,23 @@ for (i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function() {
 		let input = this.id;
 		//console.log(input);
-		//console.log(column);
+		//if (сolumns[column] != 0) {}
+		//if (сolumns[column-1] <= сolumns[column]) {}
 		if (input == 'left' && x>0) {
 			column += -1;
 			x += -25;
 			square.style.left = x+"px";
-		} else if (input == 'right' && x<250) {
+		}
+		if (input == 'right' && x<250) {
 			column += 1;
 			x += 25;
 			square.style.left = x+"px";
 		}
+		console.log(column);
+		//console.log(сolumns[column]);
 	});
 }
 
 squareCreation();
-setInterval(move, 500);
+let timerId = setInterval(move, 50);
 //start();
