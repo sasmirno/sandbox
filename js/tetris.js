0e5b4c029129124ed88afceb817b38370ec9caf0
sasmirno <sasmirno1@gmail.com>
2023-01-09 21:00:29 +0300

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -2,31 +2,31 @@ field = document.querySelector(".tetris_field");
 buttons = document.querySelectorAll(".tetris_btn");
 
 
-let x = 125;
+let x = 5;
 let y = 0;
-let column = 5;
-let row = 0;
-let сolumns = [500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500];
+//let column = 5;
+//let row = 0;
+let сolumns = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20];
 
 /*function start() {
 	setInterval(move, 50);
 }*/
 
 function move() {
-	console.log(row);
-	if (y<сolumns[column]) {
-		square.style.top = y+"px";
-		y += 25;
-		row += 1;
-	} else if (сolumns[column] == 0) {
+	//console.log(y);
+	if (y<сolumns[x]) {
+		square.style.top = y*25+"px";
+		y += 1;
+		//row += 1;
+	} else if (сolumns[x] == 0) {
 		console.log('stop');
 		clearInterval(timerId);
 	} else {
-		сolumns[column] += -25
-		x = 125;
+		сolumns[x] += -1
+		x = 5;
 		y = 0;
-		row = 0;
-		column = 5;
+		//row = 0;
+		//column = 5;
 		squareCreation();
 		//console.log(сolumns);
 	}
@@ -35,7 +35,7 @@ function move() {
 function squareCreation() {
 	square=document.createElement("div");
 	square.style.position = "absolute";
-	square.style.left = x+"px";
+	square.style.left = x*25+"px";
 	square.style.width = "25px";
 	square.style.height = "25px";
 	square.style.background = "red";
@@ -47,20 +47,29 @@ for (i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function() {
 		let input = this.id;
 		//console.log(input);
-		//if (сolumns[column] != 0) {}
-		//if (сolumns[column-1] <= сolumns[column]) {}
+		//if (сolumns[x] != 0) {}
+		/*if (сolumns[x-1] <= сolumns[x]) {
+			console.log(сolumns[x-1]);
+		}
+		if (сolumns[x+1] <= сolumns[x]) {
+			console.log(сolumns[x+1]);
+		}*/
 		if (input == 'left' && x>0) {
-			column += -1;
-			x += -25;
-			square.style.left = x+"px";
+			if (сolumns[x-1] > y) {
+				x += -1;
+				square.style.left = x*25+"px";
+			}
+			//console.log(сolumns[x-1]);
 		}
-		if (input == 'right' && x<250) {
-			column += 1;
-			x += 25;
-			square.style.left = x+"px";
+		if (input == 'right' && x<10) {
+			if (сolumns[x+1] > y) {
+				x += 1;
+				square.style.left = x*25+"px";
+			}
+			//console.log(сolumns[x+1]);
 		}
-		console.log(column);
-		//console.log(сolumns[column]);
+		//console.log(x);
+		//console.log(сolumns[x]);
 	});
 }
 
