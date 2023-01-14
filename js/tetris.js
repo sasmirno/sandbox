2f98de98f08b508c4028cdb3375a852e07d2518c
sasmirno <sasmirno1@gmail.com>
2023-01-14 21:05:53 +0300

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -6,7 +6,6 @@ buttons = document.querySelectorAll(".tetris_btn");
 let matrix = [
 	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,10 +19,12 @@ let matrix = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
 ];
 
 let x = 5;
@@ -34,30 +35,47 @@ function move() {
 	//let cell;
 	for (let key in matrix) {
 		for (i=0; i<11; i++) {
-			if (matrix[key][i] == 1 && [parseInt(key)+1][i] != 3) {
+			if (matrix[key][i] == 1) {
 				matrix[key][i] = 0;
 				matrix[parseInt(key)+1][i] = 2;
 				//console.log(matrix[parseInt(key)+1][i]);
 				//console.log(parseInt(key)+1);
 				//console.log(i);
+			} else {
+				//clearInterval(timerId);
+				//console.log('i');
 			}
 		}
 		for (i=0; i<11; i++) {
-			if (matrix[key][i] == 2 && [parseInt(key)+1][i] != 3) {
-				//matrix[parseInt(key)-1][i] = 0;
-				matrix[key][i] = 1;
-				//matrix[parseInt(key)+1][i] = 2;
-				//console.log(matrix[parseInt(key)+1][i]);
-				//console.log(parseInt(key)+1);
-				//console.log(key);
-				//cell = key;
+			if (key < matrix.length) {
+				if (matrix[key][i] == 2) {
+					if (matrix[parseInt(key)+1][i] == 0) {
+						matrix[key][i] = 1;
+					} else {
+						matrix[key][i] = 3;
+						clearInterval(timerId);
+						matrix[0][5] = 1;
+						move();
+					}
+					//console.log(matrix[parseInt(key)+1][i]);
+					//console.log(parseInt(key)+1);
+					//console.log(key);
+					//cell = key;
+				} else {
+					//matrix[key][i] = 3;
+				}
 			}
-		}
+		}/**/
 	}
 	visualization();
 	/*if (cell < 19) {
 		setTimeout(move, 50);
-	}*/
+	} else {
+		matrix[0][5] = 1;
+		cell = 0;
+		//move();
+	}
+	*/
 	//clearInterval(timerId);
 }
 console.log(matrix);
@@ -66,26 +84,22 @@ console.log(matrix);
 function visualization() {
 	for (let key in matrix) {
 		for (i=0; i<11; i++) {
-			//if (matrix[key][i] == 1) {}
-			//let g = matrix[key][i];
-			//console.log(g);
 			switch (matrix[key][i]) {
 				case 0:
-					//console.log('0');
+					//console.log(key*11+i);
+					cells[key*11+i].style.background="white";
 				break;
 				case 1:
 					//console.log(matrix[key][i]);
-					console.log(key);
-					console.log(i);
-					//console.log(cells[i].length);
-					//console.log(matrix[key][i]);
-					cells[i].style.background="red";
-				break;
-				case 3:
-					//console.log(matrix[key][i]);
 					//console.log(key);
 					//console.log(i);
-					//console.log('3');
+					//console.log(cells[i].length);
+					//console.log(key*11+i);
+					cells[key*11+i].style.background="red";
+				break;
+				case 3:
+					//console.log(key*11+i);
+					cells[key*11+i].style.background="purple";
 				break;
 				default:
 			}/**/
@@ -94,7 +108,7 @@ function visualization() {
 }
 
 // Кнопки управления
-for (i = 0; i < buttons.length; i++) {
+/*for (i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function() {
 		let input = this.id;
 		//console.log(input);
@@ -115,9 +129,9 @@ for (i = 0; i < buttons.length; i++) {
 		//console.log(x);
 		//console.log(сolumns[x]);
 	});
-}
+}*/
 
 visualization();
-//move();
+move();
 //shapeCreation();
-//let timerId = setInterval(move, 50);
+let timerId = setInterval(move, 50);
