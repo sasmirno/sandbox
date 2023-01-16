811aece65cc34b8b8f1befff47001d9cfd1e40fe
sasmirno <sasmirno1@gmail.com>
2023-01-16 20:56:31 +0300

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -4,106 +4,75 @@ buttons = document.querySelectorAll(".tetris_btn");
 
 
 let matrix = [
-	[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+	0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
 
-let x = 5;
-let y = 0;
-let сolumns = [20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20];
-
 function move() {
-	//let cell;
 	for (let key in matrix) {
-		for (i=0; i<11; i++) {
-			if (matrix[key][i] == 1) {
-				matrix[key][i] = 0;
-				matrix[parseInt(key)+1][i] = 2;
-				//console.log(matrix[parseInt(key)+1][i]);
-				//console.log(parseInt(key)+1);
-				//console.log(i);
+		if (matrix[key] == 1) {
+			if (matrix[parseInt(key)+11] == 0) {
+				matrix[key] = 0;
+				matrix[parseInt(key)+11] = 2;
+			} else if (matrix[parseInt(key)-11] == undefined) {
+				clearInterval(timerId);
+				console.log('stop');
 			} else {
-				//clearInterval(timerId);
-				//console.log('i');
+				matrix[key] = 'purple';
+				matrix[5] = 1;
 			}
 		}
-		for (i=0; i<11; i++) {
-			if (key < matrix.length) {
-				if (matrix[key][i] == 2) {
-					if (matrix[parseInt(key)+1][i] == 0) {
-						matrix[key][i] = 1;
-					} else {
-						matrix[key][i] = 3;
-						clearInterval(timerId);
-						matrix[0][5] = 1;
-						move();
-					}
-					//console.log(matrix[parseInt(key)+1][i]);
-					//console.log(parseInt(key)+1);
-					//console.log(key);
-					//cell = key;
-				} else {
-					//matrix[key][i] = 3;
-				}
-			}
-		}/**/
+		if (matrix[key] == 2) {
+			matrix[key] = 1;
+		}
 	}
 	visualization();
-	/*if (cell < 19) {
-		setTimeout(move, 50);
-	} else {
-		matrix[0][5] = 1;
-		cell = 0;
-		//move();
-	}
-	*/
-	//clearInterval(timerId);
+	//console.log('loop');
 }
-console.log(matrix);
+
+//console.log(matrix);
 //console.log(cells);
 
 function visualization() {
 	for (let key in matrix) {
-		for (i=0; i<11; i++) {
-			switch (matrix[key][i]) {
-				case 0:
-					//console.log(key*11+i);
-					cells[key*11+i].style.background="white";
-				break;
-				case 1:
-					//console.log(matrix[key][i]);
-					//console.log(key);
-					//console.log(i);
-					//console.log(cells[i].length);
-					//console.log(key*11+i);
-					cells[key*11+i].style.background="red";
-				break;
-				case 3:
-					//console.log(key*11+i);
-					cells[key*11+i].style.background="purple";
-				break;
-				default:
-			}/**/
-		}
+		switch (matrix[key]) {
+			case 0:
+				//console.log(key);
+				cells[key].style.background="white";
+			break;
+			case 1:
+				//console.log(matrix[key]);
+				//console.log(key);
+				//console.log(i);
+				//console.log(cells[i].length);
+				//console.log(key*11+i);
+				cells[key].style.background="red";
+			break;
+			case 'purple':
+				//console.log(key*11+i);
+				cells[key].style.background="purple";
+			break;
+			default:
+		}/**/
+		//console.log(matrix[key]);
 	}
 }
 
@@ -131,7 +100,7 @@ function visualization() {
 	});
 }*/
 
-visualization();
-move();
+//visualization();
+//move();
 //shapeCreation();
 let timerId = setInterval(move, 50);
