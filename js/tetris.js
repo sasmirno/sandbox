40d9d79fff24ed24d2bd6c50348aa9ae68c55d7a
sasmirno <sasmirno1@gmail.com>
2023-01-31 21:04:22 +0300

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -4,79 +4,49 @@ buttons = document.querySelectorAll(".tetris_btn");
 
 // Имитация игрового поля
 let matrix = [
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
 
 // Копия игрового поля
 let buffer = Array.from(matrix);
 
 // Создание фигуры
-function figure(a, b, c, d, l, r) {
-	buffer[a] = 2;
-	buffer[b] = 2;
-	buffer[c] = 2;
-	buffer[d] = 2;
+function figure(a, b, c, d, l, r, clr) {
+	buffer[a] = clr;
+	buffer[b] = clr;
+	buffer[c] = clr;
+	buffer[d] = clr;
 	left = l;
 	right = r;/**/
 }
 
 function figureCreation() {
 
-	return figure(4, 5, 6, 16, -15, 15);
+	//return figure(3, 4, 5, 6, -3, 3, 1);
+	return figure(4, 5, 6, 15, -4, 3, 2);
+	//return figure(4, 5, 14, 15, -4, 4, 3);
+	//return figure(4, 5, 6, 16, -4, 3, 4);
+	//return figure(4, 5, 15, 16, -4, 3, 5);
 
-	/*buffer[3] = 1;
-	buffer[4] = 1;
-	buffer[5] = 1;
-	buffer[6] = 1;
-	left = -10;
-	right = 15;*/
-
-	/*buffer[4] = 2;
-	buffer[5] = 2;
-	buffer[6] = 2;
-	buffer[16] = 2;
-	left = -15;
-	right = 15;*/
-
-	/*buffer[4] = 3;
-	buffer[5] = 3;
-	buffer[15] = 3;
-	buffer[16] = 3;
-	left = -15;
-	right = 20;*/
-
-	/*buffer[4] = 4;
-	buffer[5] = 4;
-	buffer[15] = 4;
-	buffer[26] = 4;
-	left = -15;
-	right = 20;*/
-
-	/*buffer[4] = 5;
-	buffer[5] = 5;
-	buffer[16] = 5;
-	buffer[17] = 5;
-	left = -15;
-	right = 15;*/
 }
 
 // Движение фигуры вниз
@@ -85,7 +55,7 @@ function move() {
 	let free = true;
 	for (let key in matrix) {
 		if (isNaN(matrix[key]) == false && matrix[key] != 0) {
-			if (isNaN(matrix[parseInt(key)+11]) == true) {
+			if (isNaN(matrix[parseInt(key)+10]) == true) {
 				free = false;
 			}
 		}
@@ -94,12 +64,12 @@ function move() {
 		// Ищем все числа кроме нуля в массиве игрового поля
 		if (isNaN(matrix[key]) == false && matrix[key] != 0) {
 			if (free == true) {
-				buffer[parseInt(key)+11] = matrix[key];
-				if (matrix[parseInt(key)-11] == 0 || matrix[parseInt(key)-11] == undefined) {
+				buffer[parseInt(key)+10] = matrix[key];
+				if (matrix[parseInt(key)-10] == 0 || matrix[parseInt(key)-10] == undefined) {
 					// Если верхния ячейка свободна обнуляем текущую ячейку
 					buffer[key] = 0;
 				}
-			} else if (matrix[parseInt(key)-11] == undefined) {
+			} else if (matrix[parseInt(key)-10] == undefined) {
 				// Если верхния ячейка отсуствует останавливаем таймер
 				clearInterval(timerId);
 				gameOver();
@@ -151,16 +121,17 @@ for (i = 0; i < buttons.length; i++) {
 					}
 				}
 			}
-			for (let key in matrix) {
-				if (isNaN(matrix[key]) == false && matrix[key] != 0) {
-					if (free == true) {
-						center -= 1;
+			// Если свободное место есть двигаем фигуру влево
+			if (free == true) {
+				center -= 1;
+				for (let key in matrix) {
+					if (isNaN(matrix[key]) == false && matrix[key] != 0) {
 						buffer[parseInt(key)-1] = matrix[key];
 						if (matrix[parseInt(key)+1] == 0 || matrix[parseInt(key)+1] == undefined || isNaN(matrix[parseInt(key)+1]) == true) {
 							buffer[key] = 0;
 						}
 					}
-				}/**/
+				}
 			}
 		}
 		if (input == 'right' && center<right) {
@@ -173,21 +144,22 @@ for (i = 0; i < buttons.length; i++) {
 					}
 				}
 			}
-			for (let key in matrix) {
-				if (isNaN(matrix[key]) == false && matrix[key] != 0) {
-					if (free == true) {
-						center += 1;
+			// Если свободное место есть двигаем фигуру вправо
+			if (free == true) {
+				center += 1;
+				for (let key in matrix) {
+					if (isNaN(matrix[key]) == false && matrix[key] != 0) {
 						buffer[parseInt(key)+1] = matrix[key];
 						if (matrix[parseInt(key)-1] == 0 || matrix[parseInt(key)-1] == undefined || isNaN(matrix[parseInt(key)-1]) == true) {
 							buffer[key] = 0;
 						}
 					}
-				}/**/
+				}
 			}
 		}
 		if (input == 'turn') {
 			let free = true;
-			let position = 11*iteration+center/4;
+			let position = 10*iteration+center;
 			//console.log(position);
 			/*for (let key in matrix) {
 				if (isNaN(matrix[key]) == false && matrix[key] != 0) {
@@ -216,34 +188,54 @@ for (i = 0; i < buttons.length; i++) {
 					}
 				}
 			}
+			// Фигура ''''
+			/*let figure0 = [3, 4, 5, 6, -3, 3, 1];
+			let figure90 = [-6, 4, 14, 24, -4, 5, 1];
+			let figure180 = [3, 4, 5, 6, -3, 3, 1];
+			let figure270 = [-5, 5, 15, 25, -5, 4, 1];*/
 
-			let figure0 = [4, 5, 6, 16, -15, 15];
-			//let figure0 = [15, 16, 17, 27, -15, 15];
-			let figure90 = [6, 16, 17, 28, -20, 15];
-			//let figure90 = [5, 15, 16, 27, -15, 20];
-			let figure180 = [16, 26, 27, 28, -15, 15];
-			//let figure180 = [5, 15, 16, 17, -15, 15];
-			let figure270 = [4, 15, 16, 26, -15, 20];
-			//let figure270 = [5, 16, 17, 27, -20, 15];
+			// Фигура '|'
+			let figure0 = [4, 5, 6, 15, -4, 3, 2];
+			let figure90 = [-5, 4, 5, 15, -4, 4, 2];
+			let figure180 = [-5, 4, 5, 6, -4, 3, 2];
+			let figure270 = [-5, 6, 5, 15, -5, 3, 2];/**/
+
+			// Фигура ||
+			/*let figure0 = [4, 5, 14, 15, -4, 4, 3];
+			let figure90 = [4, 5, 14, 15, -4, 4, 3];
+			let figure180 = [4, 5, 14, 15, -4, 4, 3];
+			let figure270 = [4, 5, 14, 15, -4, 4, 3];*/
+
+			// Фигура |''
+			/*let figure0 = [4, 5, 6, 16, -4, 3, 4];
+			let figure90 = [-5, 5, 14, 15, -4, 4, 4];
+			let figure180 = [-6, 4, 5, 6, -4, 3, 4];
+			let figure270 = [-5, -4, 5, 15, -5, 3, 4];*/
+
+			// Фигура .|'
+			/*let figure0 = [4, 5, 15, 16, -4, 3, 5];
+			let figure90 = [-5, 4, 5, 14, -4, 4, 5];
+			let figure180 = [4, 5, 15, 16, -4, 3, 5];
+			let figure270 = [-5, 4, 5, 14, -4, 4, 5];*/
 
 			switch(orientation) {
 				case 0:
 					check(figure90[0]+position, figure90[1]+position, figure90[2]+position, figure90[3]+position);
 					if (free == true) {
 						zeroing();
-						figure(figure90[0]+position, figure90[1]+position, figure90[2]+position, figure90[3]+position, figure90[4], figure90[5]);
+						figure(figure90[0]+position, figure90[1]+position, figure90[2]+position, figure90[3]+position, figure90[4], figure90[5], figure90[6]);
 						orientation = 90;
 					}
 				break;
 				case 90:
-					if (center == -20) {
-						position += 1;
-						center += 4;
+					if (center == 4) {
+						position -= 1;
+						center -= 1;
 					}
 					check(figure180[0]+position, figure180[1]+position, figure180[2]+position, figure180[3]+position);
 					if (free == true) {
 						zeroing();
-						figure(figure180[0]+position, figure180[1]+position, figure180[2]+position, figure180[3]+position, figure180[4], figure180[5]);
+						figure(figure180[0]+position, figure180[1]+position, figure180[2]+position, figure180[3]+position, figure180[4], figure180[5], figure180[6]);
 						orientation = 180;
 					}
 				break;
@@ -251,19 +243,19 @@ for (i = 0; i < buttons.length; i++) {
 					check(figure270[0]+position, figure270[1]+position, figure270[2]+position, figure270[3]+position);
 					if (free == true) {
 						zeroing();
-						figure(figure270[0]+position, figure270[1]+position, figure270[2]+position, figure270[3]+position, figure270[4], figure270[5]);
+						figure(figure270[0]+position, figure270[1]+position, figure270[2]+position, figure270[3]+position, figure270[4], figure270[5], figure270[6]);
 						orientation = 270;
 					}
 				break;
 				case 270:
-					if (center == 20) {
-						position -= 1;
-						center -= 4;
+					if (center == -5) {
+						position += 1;
+						center += 1;
 					}
 					check(figure0[0]+position, figure0[1]+position, figure0[2]+position, figure0[3]+position);
 					if (free == true) {
 						zeroing();
-						figure(figure0[0]+position, figure0[1]+position, figure0[2]+position, figure0[3]+position, figure0[4], figure0[5]);
+						figure(figure0[0]+position, figure0[1]+position, figure0[2]+position, figure0[3]+position, figure0[4], figure0[5], figure0[6]);
 						orientation = 0;
 					}
 				break;
@@ -271,7 +263,7 @@ for (i = 0; i < buttons.length; i++) {
 			//console.log(orientation);
 			//console.log('turn');
 		}
-		//console.log(center);
+		console.log(center);
 		for (let key in buffer) {
 			matrix[key] = buffer[key];
 		}
@@ -318,98 +310,98 @@ let timerId = setInterval(move, 500);
 // Экран конца игры
 function gameOver() {
 	// Буква G
-	buffer[177] = 'gg';
-	buffer[179] = 'gg';
-	buffer[187] = 'gg';
-	buffer[190] = 'gg';
+	buffer[161] = 'gg';
+	buffer[163] = 'gg';
+	buffer[170] = 'gg';
+	buffer[180] = 'gg';
+	buffer[173] = 'gg';
+	buffer[174] = 'gg';
+	buffer[184] = 'gg';
 	buffer[191] = 'gg';
-	buffer[198] = 'gg';
-	buffer[202] = 'gg';
-	buffer[210] = 'gg';
-	buffer[211] = 'gg';
-	buffer[212] = 'gg';
+	buffer[192] = 'gg';
+	buffer[193] = 'gg';
 	// Буква A
-	buffer[122] = 'gg';
-	buffer[123] = 'gg';
-	buffer[124] = 'gg';
-	buffer[125] = 'gg';
-	buffer[132] = 'gg';
-	buffer[135] = 'gg';
+	buffer[111] = 'gg';
+	buffer[112] = 'gg';
+	buffer[113] = 'gg';
+	buffer[114] = 'gg';
+	buffer[120] = 'gg';
+	buffer[130] = 'gg';
+	buffer[141] = 'gg';
+	buffer[142] = 'gg';
 	buffer[143] = 'gg';
-	buffer[146] = 'gg';
-	buffer[155] = 'gg';
+	buffer[144] = 'gg';
+	buffer[123] = 'gg';
+	buffer[133] = 'gg';
+	// Буква M
+	buffer[50] = 'gg';
+	buffer[51] = 'gg';
+	buffer[52] = 'gg';
+	buffer[53] = 'gg';
+	buffer[54] = 'gg';
+	buffer[61] = 'gg';
+	buffer[72] = 'gg';
+	buffer[81] = 'gg';
+	buffer[90] = 'gg';
+	buffer[91] = 'gg';
+	buffer[92] = 'gg';
+	buffer[93] = 'gg';
+	buffer[94] = 'gg';
+	// Буква E
+	buffer[10] = 'gg';
+	buffer[20] = 'gg';
+	buffer[30] = 'gg';
+	buffer[31] = 'gg';
+	buffer[32] = 'gg';
+	buffer[33] = 'gg';
+	buffer[34] = 'gg';
+	buffer[24] = 'gg';
+	buffer[14] = 'gg';
+	buffer[22] = 'gg';
+	// Буква O
 	buffer[156] = 'gg';
 	buffer[157] = 'gg';
 	buffer[158] = 'gg';
-	// Буква M
-	buffer[55] = 'gg';
-	buffer[56] = 'gg';
-	buffer[57] = 'gg';
-	buffer[58] = 'gg';
-	buffer[59] = 'gg';
-	buffer[67] = 'gg';
-	buffer[79] = 'gg';
-	buffer[89] = 'gg';
-	buffer[99] = 'gg';
-	buffer[100] = 'gg';
-	buffer[101] = 'gg';
-	buffer[102] = 'gg';
-	buffer[103] = 'gg';
+	buffer[165] = 'gg';
+	buffer[175] = 'gg';
+	buffer[169] = 'gg';
+	buffer[179] = 'gg';
+	buffer[186] = 'gg';
+	buffer[187] = 'gg';
+	buffer[188] = 'gg';
+	// Буква V
+	buffer[95] = 'gg';
+	buffer[96] = 'gg';
+	buffer[97] = 'gg';
+	buffer[108] = 'gg';
+	buffer[119] = 'gg';
+	buffer[128] = 'gg';
+	buffer[137] = 'gg';
+	buffer[136] = 'gg';
+	buffer[135] = 'gg';
 	// Буква E
-	buffer[11] = 'gg';
+	buffer[55] = 'gg';
+	buffer[65] = 'gg';
+	buffer[75] = 'gg';
+	buffer[76] = 'gg';
+	buffer[77] = 'gg';
+	buffer[78] = 'gg';
+	buffer[79] = 'gg';
+	buffer[67] = 'gg';
+	buffer[69] = 'gg';
+	buffer[59] = 'gg';
+	// Буква R
+	buffer[6] = 'gg';
+	buffer[9] = 'gg';
+	buffer[18] = 'gg';
+	buffer[17] = 'gg';
 	buffer[15] = 'gg';
-	buffer[22] = 'gg';
-	buffer[24] = 'gg';
-	buffer[26] = 'gg';
-	buffer[33] = 'gg';
-	buffer[34] = 'gg';
+	buffer[27] = 'gg';
+	buffer[25] = 'gg';
 	buffer[35] = 'gg';
 	buffer[36] = 'gg';
 	buffer[37] = 'gg';
-	// Буква O
-	buffer[172] = 'gg';
-	buffer[173] = 'gg';
-	buffer[174] = 'gg';
-	buffer[182] = 'gg';
-	buffer[186] = 'gg';
-	buffer[193] = 'gg';
-	buffer[197] = 'gg';
-	buffer[205] = 'gg';
-	buffer[206] = 'gg';
-	buffer[207] = 'gg';
-	// Буква V
-	buffer[105] = 'gg';
-	buffer[106] = 'gg';
-	buffer[107] = 'gg';
-	buffer[119] = 'gg';
-	buffer[131] = 'gg';
-	buffer[141] = 'gg';
-	buffer[149] = 'gg';
-	buffer[150] = 'gg';
-	buffer[151] = 'gg';
-	// Буква E
-	buffer[61] = 'gg';
-	buffer[65] = 'gg';
-	buffer[72] = 'gg';
-	buffer[74] = 'gg';
-	buffer[76] = 'gg';
-	buffer[83] = 'gg';
-	buffer[84] = 'gg';
-	buffer[85] = 'gg';
-	buffer[86] = 'gg';
-	buffer[87] = 'gg';
-	// Буква R
-	buffer[7] = 'gg';
-	buffer[17] = 'gg';
-	buffer[19] = 'gg';
-	buffer[10] = 'gg';
-	buffer[28] = 'gg';
-	buffer[30] = 'gg';
-	buffer[20] = 'gg';
+	buffer[38] = 'gg';
 	buffer[39] = 'gg';
-	buffer[40] = 'gg';
-	buffer[41] = 'gg';
-	buffer[42] = 'gg';
-	buffer[43] = 'gg';
 	visualization();
 }
