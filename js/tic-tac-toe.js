5e92b02f3ab6e3fe147671d21f8d30d071e106aa
sasmirno <sasmirno1@gmail.com>
2023-03-02 21:04:59 +0300

    фпр

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,5 +1,57 @@
 cells = document.querySelectorAll(".tic-tac-toe__field_cell");
 
+let player;
+let ai;
+
+let field = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// Рисование знака в ячейке при наведенее на неё
+for  (i = 0; i < cells.length; i++) {
+	cells[i].addEventListener("mouseover", function() {
+		if (field[this.id] === 0) {
+			if (player === 'x') {
+				cells[this.id].innerHTML="&#215;";
+			} else {
+				cells[this.id].innerHTML="&#9675;";
+			}
+			cells[this.id].style.color="grey";
+		}
+	});
+	cells[i].addEventListener("mouseout", function() {
+		if (field[this.id] === 0) {
+			cells[this.id].style.color="white";
+		}
+	});
+}
+
+// Установка знака в игровую ячейку
+for  (i = 0; i < cells.length; i++) {
+	cells[i].addEventListener("click", function() {
+		field[this.id] = player;
+		cells[this.id].style.color="black";
+		visualization();
+	});
+}
+//console.log(field);
+
+// Проверка на выигрышные линии или ничью
+
+// Написать мега-супер-искусственный интеллект для игры
+
+// Функция визуализации на сайт
+function visualization() {
+	for (let key in field) {
+		switch (field[key]) {
+			case 'x':
+				cells[key].innerHTML="&#215;";
+			break;
+			case 'o':
+				cells[key].innerHTML="&#9675;";
+			break;
+		}
+	}
+}
+
 // Экран начала игры
 function play() {
 	let play = document.querySelector('#play');
@@ -28,39 +80,6 @@ function play() {
 		}
 	}
 }
-
-let player;
-let ai;
-
-let field = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-// Установка знака в игровую ячейку
-for  (i = 0; i < cells.length; i++) {
-	cells[i].addEventListener("click", function() {
-		field[this.id] = player;
-		visualization();
-	});
-}
-//console.log(field);
-
-// Функция визуализации
-function visualization() {
-	for (let key in field) {
-		switch (field[key]) {
-			case 0:
-				//cells[key].style.background="white";
-			break;
-			case 'x':
-				cells[key].innerHTML="&#215;";
-			break;
-			case 'o':
-				cells[key].innerHTML="&#9675;";
-			break;
-		}
-	}
-}
-
-
 // Экран конца игры
 function gameOver() {
 	let gameOver = document.querySelector('#gameOver');
