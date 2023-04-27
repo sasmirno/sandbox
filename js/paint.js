ebf1a3dd639419f1c261de1cb33d2e5ab612227c
sasmirno <sasmirno1@gmail.com>
2023-04-27 20:40:09 +0300

    agr

--- js/paint.js
+++ js/paint.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('drawing-board'); // Холст
 const toolbar = document.getElementById('toolbar'); // Панель управления
+const wrapper  = document.querySelector('.wrapper');
 
 const ctx = canvas.getContext('2d');
 
@@ -15,50 +16,54 @@ let lineWidth = 5;
 let startX;
 let startY;
 
-// Кнопка очистки холста
 toolbar.addEventListener('click', e => {
+	// Кнопка очистки холста
 	if (e.target.id === 'clear') {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	}
+	// Уменьшение толщены линии
+	if (e.target.id === 'downWidth') {
+		if (lineWidth !== 1) {
+			lineWidth -= 1;
+			document.querySelector("#lineWidth").innerHTML = lineWidth;
+		}
+	}
+	// Увеличение толщены линии
+	if (e.target.id === 'upWidth') {
+		if (lineWidth <= 15) {
+			lineWidth += 1;
+			document.querySelector("#lineWidth").innerHTML = lineWidth;
+		}
+	}
 });
-// Выбор цвета и толщины
+
+// Выбор цвета
 toolbar.addEventListener('change', e => {
 	if(e.target.id === 'stroke') {
 		ctx.strokeStyle = e.target.value;
 	}
-	if(e.target.id === 'lineWidth') {
-		lineWidth = e.target.value;
-	}
 });
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener('pointerdown', (e) => {
 	isPainting = true;
 	startX = e.clientX;
 	startY = e.clientY;
 });
 
-canvas.addEventListener('mouseup', e => {
+wrapper.addEventListener('pointerup', e => {
 	isPainting = false;
 	ctx.stroke();
 	ctx.beginPath();
 });
 
-/*canvas.addEventListener('mouseout', e => {
-	isPainting = false;
-	ctx.stroke();
-	ctx.beginPath();
-});*/
-
 const draw = (e) => {
 	if(!isPainting) {
 		return;
 	}
-
 	ctx.lineWidth = lineWidth;
 	ctx.lineCap = 'round';
-
 	ctx.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
 	ctx.stroke();
 }
 
-canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('pointermove', draw);
