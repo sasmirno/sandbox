84f64a92518316778bba36df8874ade0aa38ce87
sasmirno <sasmirno1@gmail.com>
2023-05-04 20:40:12 +0300

    Дополнил главную страницу

--- js/app.js
+++ js/app.js
@@ -7,3 +7,17 @@ menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('burger_active');
 	lock.classList.toggle('burger_lock');
 })
+
+// accordion
+let acc = document.getElementsByClassName("accordion");
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+		this.classList.toggle("accordion_open");
+		let panel = this.nextElementSibling;
+		if (panel.style.maxHeight){
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		} 
+	});
+}
